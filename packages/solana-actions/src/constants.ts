33fdd1818fc3962337ce1b278ad0c0e57dd247f7
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-07-23 16:33:46 -0400

    feat: added middleware headers

--- packages/solana-actions/src/constants.ts
+++ packages/solana-actions/src/constants.ts
@@ -4,11 +4,20 @@ export const HTTPS_PROTOCOL = "https:";
 /** Program Id for the SPL Memo program */
 export const MEMO_PROGRAM_ID = "MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr";
 
-/**  */
+/**
+ * Specification declared URL query parameter to detect blinks via interstitial website URLs.
+ *
+ * Note: The `action` query parameter should begin with the `solana-action:` protocol identifier
+ *
+ * Example: `https://dial.to/?action=solana-action:https://jupiter.dial.to/swap/SOL-Bonk`
+ */
 export const BLINKS_QUERY_PARAM = "action";
 
 /**
- * Standard headers
+ * Standard headers for use within frameworks that use the native `HeadersInit` (like NextJS)
+ *
+ * Note: `Access-Control-Allow-Origin=*` should ONLY be set on your Actions API routes and `actions.json`.
+ * Setting "allow origin to any" on other routes on your server is bad practice and should be avoided.
  */
 export const ACTIONS_CORS_HEADERS: HeadersInit = {
   "Access-Control-Allow-Origin": "*",
@@ -16,4 +25,22 @@ export const ACTIONS_CORS_HEADERS: HeadersInit = {
   "Access-Control-Allow-Headers":
     "Content-Type, Authorization, Content-Encoding, Accept-Encoding",
   "Content-Type": "application/json",
+};
+
+/**
+ * Standard headers for use within frameworks that use middleware to handle CORS headers
+ * (like Hono, Express, and Fastify)
+ *
+ * Note: `origin=*` should ONLY be set on your Actions API routes and `actions.json`.
+ * Setting "allow origin to any" on other routes on your server is bad practice and should be avoided.
+ */
+export const ACTIONS_CORS_HEADERS_MIDDLEWARE = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Content-Encoding",
+    "Accept-Encoding",
+  ],
 };
